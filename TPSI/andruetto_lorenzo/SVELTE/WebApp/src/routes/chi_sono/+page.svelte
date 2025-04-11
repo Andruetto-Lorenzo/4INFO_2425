98ab8211ce7ef2d10775066b588503bdd6a1c792
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-04-11 12:39:27 +0200

    aggiunta es svelte 09

---
+++ TPSI/andruetto_lorenzo/SVELTE/WebApp/src/routes/chi_sono/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import WhoAmI from "../../lib/components/who_am_i.svelte";
+</script>
+
+<h1>Pagina: Chi Sono</h1>
+<WhoAmI nome="Lorenzo" cognome="Andruetto" eta="17" src='https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png'
+width="160" height="160"/>
