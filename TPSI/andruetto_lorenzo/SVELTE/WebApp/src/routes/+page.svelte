f5a99b5ba31b40e6a39e6fa17889e4eceb5f3c83
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-04-11 12:18:33 +0200

    aggiunto es 08 svelte

---
+++ TPSI/andruetto_lorenzo/SVELTE/WebApp/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div>
+    <h1>
+        WELCOME to my WebApp
+    </h1>
+</div>
+
+<style>
+    h1 {
+        color: blue;
+        font-size: 10vh;
+    }
+    div{
+        height: 75vh;
+        border: 3px solid blue;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
