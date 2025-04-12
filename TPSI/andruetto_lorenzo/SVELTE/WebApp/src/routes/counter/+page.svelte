a5d935fcd855fb7542639262bf4c6601166633ed
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-04-12 18:26:54 +0200

    aggiunta es svelte 13

--- TPSI/andruetto_lorenzo/SVELTE/WebApp/src/routes/counter/+page.svelte
+++ TPSI/andruetto_lorenzo/SVELTE/WebApp/src/routes/counter/+page.svelte
@@ -3,14 +3,30 @@
     import DoubleCounter from "$lib/components/double_counter.svelte";
     let counter = 0;
     let doubleCounter = 0;
+    let sottolineato = true;
+    $: sottolineato = doubleCounter % 2 == 0 ? true : false; 
+    $: doppio = counter * 2;
+    let numeri = [1,7,2,6,9,10];
 </script>
 
 <h1>Pagina Counter!!</h1>
 
-<h2>Il valore del contatore vale: {counter}</h2>
+<h2 class:sottolineato>Il valore del contatore vale: {counter}</h2>
+<h2>Il suo doppio vale: {doppio}</h2>
 
 <Counter bind:counter={counter} />
 
+<ul>
+    {#each numeri as numero, indice}
+        <li>numeri[{indice}]:{numero}</li>
+    {/each}
+</ul>
+
+<style>
+    .rosso {color: red;}
+    .verde {color: green;}
+    .sottolineato { text-decoration: underline; }
+</style>
 <h2>Il valore del secondo contatore vale: {doubleCounter}</h2>
 
 <DoubleCounter bind:doubleCounter={doubleCounter}/>
