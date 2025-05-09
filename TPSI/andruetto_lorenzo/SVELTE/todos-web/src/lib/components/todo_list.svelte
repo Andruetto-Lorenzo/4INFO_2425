f397eee69f4c4651c723804c6bf18312bf08863d
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-05-09 13:26:08 +0200

    aggiunto todo web app

---
+++ TPSI/andruetto_lorenzo/SVELTE/todos-web/src/lib/components/todo_list.svelte
@@ -0,0 +1,39 @@
+<h1>ToDos</h1>
+<div class="todo-list">
+    <div class="header">H1</div>
+    <div class="header">H2</div>
+    <div class="header">H3</div>
+    <div class="header">H4</div>
+    <div class="header">H5</div>
+
+    <!--ToDo Item 1-->
+    <div>CELLA 1</div>
+    <div>CELLA 2</div>
+    <div>CELLA 3</div>
+    <div>CELLA 4</div>
+    <div>CELLA 5</div>
+
+    <!--ToDo Item 2-->
+    <div>CELLA 1</div>
+    <div>CELLA 2</div>
+    <div>CELLA 3</div>
+    <div>CELLA 4</div>
+    <div>CELLA 5</div>
+
+    <!--ToDo Item 3-->
+    <div>CELLA 1</div>
+    <div>CELLA 2</div>
+    <div>CELLA 3</div>
+    <div>CELLA 4</div>
+    <div>CELLA 5</div>
+</div>
+
+<style>
+    .todo-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr 4fr 2fr 1fr;
+        border: 3px solid blue;
+        width: 95%;
+        margin: auto;
+    }
+</style>
